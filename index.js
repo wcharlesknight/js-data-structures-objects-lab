f7d4b77eea6f5ab85e78a24ac782ac121b778344
Charlie Knight <williamchknight@gmail.com>
2021-01-13 16:56:59 -0800

    commit tree f0599a5293f4e70ddfd55a6ffa25638b6f80320b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+// Write your solution in this file
+let driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const newDriver = { ... driver }
+
+    newDriver[key] = value 
+    return newDriver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    driver[key] = value 
+    return driver
+}
+
+function deleteFromDriverByKey(driver, key){
+    const newDriver = { ... driver }
+    
+    delete newDriver[key]
+    return newDriver
+}
+
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key]
+    return driver 
+}
+
+
